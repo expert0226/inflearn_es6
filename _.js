ccef214656eb69e070c5f0eaa57376562be153b1
Jongmin Kim <expert0226@bbros.kr>
2017-08-29 14:08:19 +0900

    _pipe

--- _.js
+++ _.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const _curry = fn => (a, ...rest) => rest.length > 0 ? fn(a, rest[0]) : b => fn(a, b);
+
+const _curryr = fn => (a, ...rest) => rest.length > 0 ? fn(a, rest[0]) : b => fn(b, a);
+
 const _each = (list, iter) => {
     for (let i = 0; i < list.length; i++) {
         iter(list[i]);
@@ -7,16 +11,36 @@ const _each = (list, iter) => {
     return list;
 };
 
-const _filter = (list, predi) => {
+const _filter = _curryr((list, predi) => {
     const new_list = [];
     _each(list, val => {
         if(predi(val)) new_list.push(val);
     });
     return new_list;
-};
+});
 
-const _map = (list, mapper) => {
+const _map = _curryr((list, mapper) => {
     const new_list = [];
     _each(list, val => new_list.push(mapper(val)));
     return new_list;
+});
+
+const _get = _curryr((obj, key) => obj == null ? undefined : obj[key]);
+
+const _rest = (list, num) => Array.from(list).slice(num || 1);
+
+const _reduce = (list, iter, memo) => {
+    if (!memo) {
+        memo = list[0];
+        list = _rest(list);
+    }
+    _each(list, val => memo= iter(memo, val));
+    return memo
+};
+
+const _pipe = (...fns) => arg => _reduce(fns, (arg, fn) => fn(arg), arg);
+
+const _go = (...args) => {
+    const fns = _rest(args);
+    return _pipe(...fns)(args[0]);
 };
